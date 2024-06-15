19d1d190b5188197bc248d72dbaab50e4228613e
Ivan Barbosa Filho <ivanbarbosafilho@gmail.com>
2024-06-15 11:57:22 -0300

    Everything ok

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 //Código do Usuário
 
-function  getHumanChoice(){ 
+function  getHumanChoice(UserAnswer){ 
 
     UserAnswer = window.prompt("What you choose?")
-
     return UserAnswer
 
 }
 
 //Código do computador
-function getComputerChoice() {
+function getComputerChoice(ComputerAnswer) {
     
     ComputerAnswer = Math.floor(Math.random() * 3) + 1;
 
@@ -27,53 +26,58 @@ function getComputerChoice() {
 
 //Pontuação
     let HumanScore = 0
+    let ComputerScore = 0
     
 
-function Playround(HumanChoice, ComputerChoice){
+    function playRound(humanChoice, computerChoice) { 
 
-    
-    for (let ComputerScore = 0; ComputerChoice > HumanChoice; ComputerScore++){
+        
 
-        if ("Rock" < "Paper"){
+        switch(humanChoice){
+            case "ROCK":
+                    if (computerChoice === "Paper"){
+                        result = "You lose! Paper beats Rock"
+                        return result
+                    } else if (computerChoice === "Scissor"){
+                        result = "You win! Rock beats Scissor"
+                        return result
+                    } else (computerChoice === "Rock");{
+                        result = "It's a tie"
+                        return result
+                    }
+            break;
+            case "SCISSOR":
+                    if (computerChoice === "Rock"){
+                               result = "You lose! Rock beats Scissor"
+                        return result
+                    } else if (computerChoice === "Paper"){
+                               result = "You win! Scissor beats Paper"
+                        return result
+                    } else (computerChoice === "Scissor");{
+                               result = "it's a tie"
+                        return result
+                    }
+            break;
+            case "PAPER":
+                    if (computerChoice === "Rock"){
+                        result = "You win! Paper beats Rock"
+                        return result
+                      }else if (computerChoice === "Paper"){
+                        result = "It's a tie"
+                        return result
+                      }else (computerChoice === "Scissor");{
+                        result = "You lose! Scissor beats Paper"
+                        return result
+                    }
+            break;        
+            } 
+      }
+      
+      const humanSelection = getHumanChoice();
+      const computerSelection = getComputerChoice();
 
-    } else if ("Paper" < "Scissor"){
-
-    } else ("Scissor" < "Rock");{
-
-    }
-
-    return "Computer Score: " + ComputerScore + " Human Score: " + HumanScore;
-    
-}
-
-console.log(Playround(getHumanChoice(), getComputerChoice()))
-
-
-
-//function Playround(HumanChoice, ComputerChoice) {
-    
-    //let HumanScore = 0
-    //let ComputerScore = 0
-
-    //if ("Rock" < "Paper"){
-        //ComputerScore + 1
-    //} else if ("Paper" < "Scissor"){
-        //ComputerScore + 1
-    //} else if ("Scissor" < "Rock"){
-        //ComputerScore + 1
-    //} else if ("Paper" === "Paper"){
-        //ComputerScore + 1, HumanScore + 1
-    //} else if ("Scissor" === "Scissor"){
-        //ComputerScore + 1, HumanScore + 1
-    //} else if ("Rock" === "Rock"){
-        //ComputerScore + 1, HumanScore + 1
-    //} else if ("Rock" > "Scissor"){
-        //HumanScore + 1
-    //} else if ("Paper" > "Rock"){
-        //HumanScore + 1
-    //} else if ("Scissor" > "Paper")
-        //HumanScore + 1
-
-    //return "Computer Score: " + ComputerScore + " Human Score: " + HumanScore
-    
-//:^}|}}}}}
+      console.log(humanSelection)
+      console.log(computerSelection)
+      
+      console.log(playRound(humanSelection, computerSelection));
+     
